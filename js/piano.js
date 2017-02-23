ff92afe8a20a37b6e1d39ae407dd35ab54d35b66
Mitzi S <contact@mitzisunshine.com>
2017-02-23 18:34:12 -0500

    Add files via upload

---
+++ js/piano.js
@@ -0,0 +1,15 @@
+function showNote(e) {
+    var note = document.getElementsByClassName("note");
+    var i;
+    const noteSound=document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const noteActive=document.querySelector(`li[data-key="${e.keyCode}"]`);
+    for (i=0; i<7; i++){
+       note[i].className = note[i].className.replace(" active", " hidden");
+    }
+    if (!noteSound) return;
+    noteSound.currentTime=0;
+    noteSound.play();
+    noteActive.className = noteActive.className.replace(" hidden"," active");
+}
+
+window.addEventListener('keydown', showNote);
