552678616d940a3b918bc5f98b38f77a6ca577a1
Mitzi S <contact@mitzisunshine.com>
2017-02-23 19:01:50 -0500

    Update piano.js

--- js/piano.js
+++ js/piano.js
@@ -1,3 +1,17 @@
+function getKey(a){
+    var note = document.getElementsByClassName("note");
+    var i;
+    const noteSound=document.querySelector(`audio[data-key="${a.keyCode}"]`);
+    const noteActive=document.querySelector(`li[data-key="${a.keyCode}"]`);
+    for (i=0; i<7; i++){
+       note[i].className = note[i].className.replace(" active", " hidden");
+    }
+    if (!noteSound) return;
+    noteSound.currentTime=0;
+    noteSound.play();
+    noteActive.className = noteActive.className.replace(" hidden"," active");
+}
+
 function showNote(e) {
     var note = document.getElementsByClassName("note");
     var i;
